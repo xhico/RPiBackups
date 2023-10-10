07bf3e2aa3a407864ec2803e6b05e36177277c41
xhico <francgf@gmail.com>
2023-10-10 19:08:52 +0100

    Remove KEEP_DAYS

--- RPiBackups.py
+++ RPiBackups.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
-import datetime
 import json
 import os
 import shutil
@@ -47,65 +46,6 @@ def setSSHConnection(copyDevice):
     return sftp
 
 
-def cleanup_old_files(copyDevice, bak_folder, localPath):
-    """
-    Clean up old files in a specified folder.
-
-    Args:
-        copyDevice (str): The name of the device.
-        bak_folder (str): The folder path where backup files are located.
-        localPath (str): The local path used as a filter for deleting files.
-
-    Returns:
-        None
-    """
-    # Remove the current date and time prefix from localPath
-    localPath = localPath.replace(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_", "")
-
-    # Get the current date and time
-    today = datetime.datetime.now()
-
-    # List to store old files
-    oldFiles = []
-
-    # Iterate through files in the backup folder
-    for filename in os.listdir(bak_folder):
-        file_path = os.path.join(bak_folder, filename)
-
-        # Get the creation time of the file
-        creation_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
-
-        # Check if the file is older than the specified number of days and matches the localPath
-        if (today - creation_time).days > config[copyDevice]["KEEP_DAYS"] and localPath in file_path:
-            oldFiles.append(file_path)
-
-    # Dictionary to store the last file per day
-    last_files_per_day = {}
-
-    # Sort the old files by file path
-    for file_path in sorted(oldFiles):
-        date = file_path.split("/")[-1].split("_")[0]
-
-        # Check if the file is the latest for the given date
-        if date not in last_files_per_day or file_path > last_files_per_day[date]:
-            last_files_per_day[date] = file_path
-
-    # Convert the dictionary values to a list of the last files per day
-    last_files_per_day = [file for _, file in last_files_per_day.items()]
-
-    # Identify files to delete
-    files_to_delete = [file for file in oldFiles if file not in last_files_per_day]
-
-    # Delete identified files
-    for file in files_to_delete:
-        os.remove(file)
-
-    # Log the number of files deleted
-    logger.info("Deleted " + str(len(files_to_delete)) + " files")
-
-    return
-
-
 def main():
     """
     Main function to copy and manage files to other devices over SSH.
@@ -136,17 +76,12 @@ def main():
             logger.info("localPath: " + localPath)
 
             # Generate a backup file name based on timestamp, hostname, and localPath
-            BAKFilename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_" + hostname + "_" + localPath.replace("/", "_SLASH_") + ".bak"
+            BAKFilename = hostname + "_" + localPath.replace("/", "_SLASH_") + ".bak"
             BAKFilePath = os.path.join(bak_folder, BAKFilename)
 
             # Copy/Send Files
-            logger.info("Copy/Send File")
-            shutil.copyfile(localPath, BAKFilePath)  # Create a local backup
-            sftp.put(localPath, BAKFilePath)  # Copy the file to the target device
-
-            # Cleanup old files
-            logger.info("Cleanup old files")
-            cleanup_old_files(copyDevice, bak_folder, BAKFilename)  # Remove old backups
+            shutil.copyfile(localPath, BAKFilePath)
+            sftp.put(localPath, BAKFilePath)
 
     return
 
