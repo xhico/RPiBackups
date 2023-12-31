ca3b212bb5cd7ba891a00ef4c5c4068b201b612c
xhico <francgf@gmail.com>
2023-12-31 16:01:01 +0000

    Add requirements.txt

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo mv /home/pi/RPiBackups/RPiBackups.service /etc/systemd/system/ && sudo systemctl daemon-reload
-python3 -m pip install paramiko --no-cache-dir
+python3 -m pip install -r /home/pi/RPiBackups/requirements.txt --no-cache-dir
 chmod +x -R /home/pi/RPiBackups/*
 mkdir /home/pi/RPiBackups/bak/
